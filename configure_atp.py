b6a536e776bcd578aa976e5ae999e3a821e9207f
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2023-05-16 14:33:06 -0600

    Add code to automatically generate ATP configs

--- configure_atp.py
+++ configure_atp.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 from pathlib import Path
 
@@ -6,15 +7,25 @@ from lm_survey.survey.survey import Survey
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("wave", type=int, help="Wave of ATP to configure")
+    parser.add_argument(
+        "wave", type=Path, nargs="+", help="Path(s) to wave of ATP to configure"
+    )
+    parser.add_argument("--base-config", type=Path, help="Path to optional base config")
     args = parser.parse_args()
 
-    survey_directory = Path("data", "ATP", f"American_Trends_Panel_W{args.wave}")
+    for wave in args.wave:
+        config_path = wave / "config.json"
 
-    survey = Survey(
-        name="ATP_W92",
-        data_filename=survey_directory / "responses.csv",
-        config_filename=survey_directory / "config.json",
-    )
+        survey = Survey(name="ATP_W92", data_filename=wave / "responses.csv")
 
-    survey.generate_dv_config(survey_directory / "config.json")
+        survey.generate_atp_config(config_path)
+
+        # This is a simple way to put some extra stuff in the config
+        if args.base_config:
+            with args.base_config.open("r") as f:
+                base_config = json.load(f)
+            with config_path.open("r") as f:
+                config = json.load(f)
+            config.extend(base_config)
+            with config_path.open("w") as f:
+                json.dump(config, f, indent=2)
